e59a5fd7abd8db71ea28f461db8d0847c58cb509
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-26 13:08:31 +0530

    variables

---
+++ variables.cs
@@ -0,0 +1,17 @@
+using System;
+ 
+namespace Sample
+{
+	class Test
+	{
+		public static void Main(string[] args)
+		{
+			int value = 10;
+
+			// Variable
+			Console.WriteLine(value);
+			// Literal
+			Console.WriteLine(50.05);
+		}
+	}
+}
