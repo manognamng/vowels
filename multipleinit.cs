2c7310a9f31f73e9f7f56064c81425a97366bb69
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-26 14:27:43 +0530

    multiple

---
+++ multipleinit.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Loop
+{
+	class ForLoop
+	{
+		public static void Main(string[] args)
+		{
+			for (int i=0, j=0; i+j<=5; i++, j++)
+			{
+				Console.WriteLine("i = {0} and j = {1}", i,j);
+			}         
+		}
+	}
+}
