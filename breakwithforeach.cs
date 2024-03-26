d50d5319ac04caaf4906b126445c60e89dba99f8
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-26 16:41:21 +0530

    breakwithforeach

---
+++ breakwithforeach.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace ForEachBreak {
+  class Program {
+    static void Main(string[] args) {
+      int[] num = { 1, 2, 3, 4, 5 };
+
+      // use of for each loop
+      foreach(int number in num) {
+
+        // terminates the loop
+         if(number==3) {
+           break; 
+           }
+
+      Console.WriteLine(number);
+        }
+    }
+  }
+}
