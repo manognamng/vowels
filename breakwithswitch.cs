c04b585988297b3e5904b53cb1e1e746b4c5a310
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-26 16:42:19 +0530

    breakwithswitch

---
+++ breakwithswitch.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace ConsoleApp1 {
+
+  class Program {
+    static void Main(string[] args) {
+      char ch='e';
+
+       switch (ch) {
+         case 'a':
+           Console.WriteLine("Vowel");
+           break;
+
+         case 'e':
+           Console.WriteLine("Vowel");
+           break;
+
+         case 'i':
+           Console.WriteLine("Vowel");
+           break;
+
+         case 'o':
+           Console.WriteLine("Vowel");
+           break;
+
+         case 'u':
+           Console.WriteLine("Vowel");
+           break;
+
+         default:
+           Console.WriteLine("Not a vowel");
+       }
+    }
+  }
+}
