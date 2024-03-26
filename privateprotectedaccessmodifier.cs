13723e97f7deb2faba7cf3bb85f502c76146218e
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-26 22:15:42 +0530

    private protected access modifier

---
+++ privateprotectedaccessmodifier.cs
@@ -0,0 +1,21 @@
+// Code in Assembly1
+using System;
+
+namespace Assembly1 {
+  public class StudentName {
+    private protected string name = "Sheeran";
+  }
+
+  //derived class of StudentName class
+  class Program1 : StudentName {
+
+    static void Main(string[] args) {
+
+      Program1 student = new Program1();
+
+      //  accessing name field from base class
+      Console.Write(student.name);
+      Console.ReadLine();
+    }
+  }
+}
