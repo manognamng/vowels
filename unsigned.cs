9231f5cd31ee9c897c6d2eaf6b23ad51c29dd2e6
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-26 10:55:17 +0530

    unsigned

---
+++ unsigned.cs
@@ -0,0 +1,12 @@
+using System;
+namespace DataType
+{
+    class ByteExample
+    {
+        public static void Main(string[] args)
+        {
+            byte age = 62;
+            Console.WriteLine(level);
+        }
+    }
+}
