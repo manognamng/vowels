04ade5db546ff4f99483ae2be93b97c8238f00d4
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-26 10:56:15 +0530

    ushort

---
+++ ushort.cs
@@ -0,0 +1,12 @@
+using System;
+namespace DataType
+{
+    class UShortExample
+    {
+        public static void Main(string[] args)
+        {
+            ushort value = 42019;
+            Console.WriteLine(value);
+        }
+    }
+}
