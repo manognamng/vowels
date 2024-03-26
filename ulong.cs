0e618c33e85ca26ffd62cb6a8d736e772a499ffe
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-26 10:58:33 +0530

    ulong

---
+++ ulong.cs
@@ -0,0 +1,12 @@
+using System;
+namespace DataType
+{
+    class ULongExample
+    {
+        public static void Main(string[] args)
+        {
+            ulong range = 17091821871L;
+            Console.WriteLine(range);
+        }
+    }
+}
