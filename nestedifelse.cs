024af77c6494c1941480d4375ec0c14ec0f6ea8f
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-26 13:50:05 +0530

    nestedifelse

---
+++ nestedifelse.cs
@@ -0,0 +1,26 @@
+using System;
+
+using System;
+
+if (boolean-expression)
+{
+	if (nested-expression-1)
+	{
+		// code to be executed
+	}
+	else
+	{
+	// code to be executed
+	}
+}
+else
+{
+	if (nested-expression-2)
+	{
+		// code to be executed
+	}
+	else
+	{
+		// code to be executed
+	}
+}
