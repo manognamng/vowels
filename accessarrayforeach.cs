f390d6b6f25e9e378d56e884c91083d3db9b014b
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-26 17:31:28 +0530

    accessarrayforeach

---
+++ accessarrayforeach.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace AccessArrayForeach {
+  class Program {
+    static void Main(string[] args) {
+      int[] numbers = {1, 2, 3};
+
+      Console.WriteLine("Array Elements: ");
+
+      foreach(int num in numbers) {
+        Console.WriteLine(num);
+      }
+
+      Console.ReadLine();
+    }
+  }
+}
+
+
