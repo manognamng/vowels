21e98368131e6ac70aab4ead20da30ff62b9c92d
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-26 13:07:25 +0530

    string2

---
+++ string2.cs
@@ -0,0 +1,16 @@
+using System;
+ 
+namespace Sample
+{
+	class Test
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Prints on ");
+			Console.WriteLine("New line");
+
+			Console.Write("Prints on ");
+			Console.Write("Same line");
+		}
+	}
+}
