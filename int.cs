70d46c4def905a73f15cb8905e20b9c5d8063c3e
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-26 10:53:00 +0530

    int

---
+++ int.cs
@@ -0,0 +1,12 @@
+using System;
+namespace DataType
+{
+    class IntExample
+    {
+        public static void Main(string[] args)
+        {
+            int score = 51092;
+            Console.WriteLine(score);
+        }
+    }
+}
