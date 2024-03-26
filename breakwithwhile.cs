4ba1725da4146f6501f239150380d88d89769927
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-26 16:10:46 +0530

    break statement with while

---
+++ breakwithwhile.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace WhileBreak {
+
+  class Program {
+    static void Main(string[] args) {
+      int i = 1;
+      while (i <= 5) {
+        Console.WriteLine(i);
+         i++;
+           if (i == 4) {
+             // terminates the loop
+             break; 
+           }
+      }
+      Console.ReadLine();
+    }
+  }
+}
