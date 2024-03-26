e22fb447253e30f1512b6bf34c66b2b1f54fb883
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-26 10:57:34 +0530

    uint

---
+++ uint.cs
@@ -0,0 +1,12 @@
+using System;
+namespace DataType
+{
+    class UIntExample
+    {
+        public static void Main(string[] args)
+        {
+            uint totalScore = 1151092;
+            Console.WriteLine(totalScore);
+        }
+    }
+}
