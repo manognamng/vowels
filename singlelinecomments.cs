49d7643e8685709cf76fa69854fbe00957bee8ad
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-26 13:20:00 +0530

    singlelinecomments

---
+++ singlelinecomments.cs
@@ -0,0 +1,14 @@
+// Hello World Program
+using System;
+
+namespace HelloWorld
+{
+	class Program
+	{
+		public static void Main(string[] args)  // Execution Starts from Main method
+		{
+			// Prints Hello World
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
