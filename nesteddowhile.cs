849e42e996ef1d97d7bb0011351bca757966a498
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-26 15:18:31 +0530

    nesteddowhile

---
+++ nesteddowhile.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Loop
+{
+	class NestedWhileLoop
+	{
+		public static void Main(string[] args)
+		{
+			int i=0;
+			do
+			{
+				int j=0;
+				do
+				{
+					Console.Write("({0},{1}) ", i,j);
+					j++;
+				} while (j<2);
+				i++;
+				Console.WriteLine();
+
+			} while (i<2);
+		}
+	}
+}
