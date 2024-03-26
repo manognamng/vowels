2973903d80faf87b2bf3ae5eae1350e12e670308
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-26 11:00:34 +0530

    double

---
+++ double.cs
@@ -0,0 +1,12 @@
+using System;
+namespace DataType
+{
+    class DoubleExample
+    {
+        public static void Main(string[] args)
+        {
+            double value = -11092.53D;
+            Console.WriteLine(value);
+        }
+    }
+}
