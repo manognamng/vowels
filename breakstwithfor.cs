1cd1872b6c3ffddddb40f1a50b9e542104ddd135
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-26 16:09:37 +0530

    break statement with for loop

---
+++ breakstwithfor.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace CSharpBreak {
+  
+  class Program {
+    static void Main(string[] args) {
+
+      for (int i = 1; i <= 4; ++i) {
+        
+        // terminates the loop
+        if (i == 3) {
+          break; 
+        }
+            	
+        Console.WriteLine(i);
+      }
+      	 
+      Console.ReadLine();
+      
+    }
+  }
+}
