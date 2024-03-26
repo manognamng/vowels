b88374cf56147136f2f18ecca5ae550a1dbd8f48
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-26 18:45:09 +0530

    publicaccessmod

---
+++ publicaccessmod.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace MyApplication {
+
+  class Student {
+    public string name = "Sheeran";
+      
+    public void print() {
+      Console.WriteLine("Hello from Student class");
+     }
+  }
+
+  class Program {
+    static void Main(string[] args) {
+    
+      // creating object of Student class
+      Student student1 = new Student();
+      
+      // accessing name field and printing it
+      Console.WriteLine("Name: " + student1.name);
+
+      // accessing print method from Student
+      student1.print();
+      Console.ReadLine();
+    }
+  }
+}
