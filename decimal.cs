a7c598d8afb5aa1824b35e4f5d60a06f9fa7e0a5
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-26 11:03:32 +0530

    decimal

---
+++ decimal.cs
@@ -0,0 +1,12 @@
+using System;
+namespace DataType
+{
+    class DecimalExample
+    {
+        public static void Main(string[] args)
+        {
+         decimal bankBalance = 53005.25M;
+            Console.WriteLine(bankBalance);
+        }
+    }
+}
