844672709eac37a0ce1a5650ed18cf9e1abce094
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-26 18:50:27 +0530

    assembly

---
+++ assembly2.cs
@@ -0,0 +1,14 @@
+// Code on Assembly1
+using System;
+
+namespace Assembly1 {
+
+  public class StudentName {
+    internal string name = "Sheeran";
+  }
+
+  class Program {
+    static void Main(string[] args) {
+    }
+  }
+}
