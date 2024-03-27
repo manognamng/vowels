5aa59a781654866397b89213c48f6b79047e3e70
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-27 14:59:27 +0530

    stringinterpolation

---
+++ stringinterpolation.cs
@@ -0,0 +1,16 @@
+using System;  
+namespace CsharpString {  
+  class Test {
+    public static void Main(string [] args) {
+
+      // create string
+      string name = "Programiz";
+
+      // string interpolation
+      string message = $"Welcome to {name}";
+      Console.WriteLine(message);
+
+      Console.ReadLine();
+    }
+  } 
+}
