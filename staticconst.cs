cd4765d84acdee19c8057ac534c8556312b0a0b2
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-27 14:29:10 +0530

    staticconst

---
+++ staticconst.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Constructor {
+
+  class Car {  
+  
+   // static constructor
+   static Car () {
+    Console.WriteLine("Static Constructor");    
+   }
+
+    // parameterless constructor
+    Car() {
+     Console.WriteLine("Default Constructor");
+   } 
+
+    static void Main(string[] args) {
+
+      // call parameterless constructor
+      Car car1 = new Car();
+
+      // call parameterless constructor again
+      Car car2 = new Car();
+
+      Console.ReadLine();
+    }
+  }
+}
