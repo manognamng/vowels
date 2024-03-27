dacab924064ebbb1d2ce8956da1df1c33dee1cd1
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-27 14:31:25 +0530

    same variable name

---
+++ thiswithsamevariablename.cs
@@ -0,0 +1,19 @@
+using System;
+ 
+namespace ThisKeyword {
+  class Test {
+
+    int num;
+    Test(int num) {
+
+      num = num;
+    }
+
+    static void Main(string[] args) {
+
+      Test t1 = new Test(4);
+      Console.WriteLine("value of num: " + t1.num);
+      Console.ReadLine();
+    }
+  }
+}
