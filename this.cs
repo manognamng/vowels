75fb6c779ffd1560e7e9c1e3064c9736b9414820
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-27 14:30:26 +0530

    this

---
+++ this.cs
@@ -0,0 +1,20 @@
+using System;
+ 
+namespace ThisKeyword {
+  class Test {
+
+    int num;
+    Test(int num) {
+      // this.num refers to the instance field
+      this.num = num;
+      Console.WriteLine("object of this: " + this);
+    }
+
+    static void Main(string[] args) {
+
+      Test t1 = new Test(4);
+      Console.WriteLine("object of t1: " + t1);
+      Console.ReadLine();
+    }
+  }
+}
