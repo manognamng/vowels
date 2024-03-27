1ee8383bef6ad08513211f89cffeb9d3a7867acd
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-27 14:40:13 +0530

    static2.cs

---
+++ static2.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace StaticKeyword {
+
+  static class Test {
+    static int a = 5;
+    static void display() {
+
+      Console.WriteLine("Static method");
+    }
+  
+    static void Main(string[] args) {
+
+      // creating object of Test
+      Test t1 = new Test();
+      Console.WriteLine(a);
+      display();
+    }
+  }
+}
