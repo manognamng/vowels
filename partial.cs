b821af7effeaa796bc8fd2815546aee6cb0648c4
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-29 23:14:57 +0530

    partial

---
+++ partial.cs
@@ -0,0 +1,16 @@
+namespace HeightWeightInfo
+{
+    class File1
+    {
+    }
+    public partial class Record
+    {
+        private int h;
+        private int w;
+        public Record(int h, int w)
+        {
+            this.h = h;
+            this.w = w;
+        }
+    }
+}
