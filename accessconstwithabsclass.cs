c5d1d8e658709b17398d51bd1a8bb60505c7541c
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-29 12:45:49 +0530

    access constructor with abs class

---
+++ accessconstwithabsclass.cs
@@ -0,0 +1,24 @@
+using System;
+namespace AbstractClass {
+  abstract class Animal {
+    
+    public Animal() {
+      Console.WriteLine("Animal Constructor");
+    }
+  }
+
+  class Dog : Animal {
+    public Dog() {
+      Console.WriteLine("Dog Constructor");
+    }   
+  }
+
+  class Program  {
+    static void Main (string [] args) {
+      // create an object of Dog class
+      Dog d1 = new Dog();  
+
+      Console.ReadLine();
+    }
+  }
+}
