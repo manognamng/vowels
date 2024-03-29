f1c62e2a725cba40e7e6f147ca95264abe3fba68
manogna <manognasony1998@gmail.comgit config --global user.email manognasony1998@gmail.com.name>
2024-03-29 23:17:07 +0530

    partial

---
+++ partial3.cs
@@ -0,0 +1,14 @@
+namespace HeightWeightInfo
+{
+    class File2
+    {
+    }
+    public partial class Record
+    {
+        public void PrintRecord()
+        {
+            Console.WriteLine("Height:"+ h);
+            Console.WriteLine("Weight:"+ w);
+        }
+    }
+}p
